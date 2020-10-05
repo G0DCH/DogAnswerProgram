c4f9f947d00c108453cb705adbebe8df36e1273a
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-10-06 01:23:39 +0900

    Update : URL이 없거나 404 에러가 떠도 그대로 진행하도록 수정

--- SeleniumCrawler.py
+++ SeleniumCrawler.py
@@ -20,7 +20,10 @@ if not os.path.isdir(outputPath):
 # 개 이름 불러오기
 dogNames = pd.read_csv(os.path.join(path, fileName), encoding = 'utf-8')
 # 웹 드라이버 실행
-browser = webdriver.Chrome(os.path.join(path, driverName))
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('disable-gpu')
+browser = webdriver.Chrome(os.path.join(path, driverName), options=options)
 header = {'Referer' : 'http://marketdata.krx.co.kr/mdi',
       'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 counter = 0
@@ -28,11 +31,19 @@ successCounter = 0
 
 # 다운로드 할 이미지 개수
 imageNumber = 10
+#canContinue = False
 
 for dogName in dogNames:
+#  if '폼피츠' in dogName:
+#    canContinue = True
+#  if not canContinue:
+#    continue
+
   url = "https://www.google.co.in/search?q="+dogName+"&source=lnms&tbm=isch"
   browser.get(url)
   imagePath = os.path.join(outputPath, dogName)
+  imageURLs = []
+  imageCount = 0
 
   if not os.path.exists(imagePath):
     os.makedirs(imagePath)
@@ -49,21 +60,36 @@ for dogName in dogNames:
     except:
       continue
     for bigX in browser.find_elements_by_xpath('//img[not(contains(@class, "rg_i"))]'):
-      print ("Succsessful Count:", successCounter)
       imageURL = bigX.get_attribute('src')
-
+      
       # 이상한거 긁어오면 다시
-      if 'base64' in str(imageURL):
+      if 'base64' in str(imageURL) or \
+        imageURL is None or \
+        imageURL in imageURLs:
         continue
       # png 타입이라고 생각하고 진행
       imageType = 'png'
       
-      req = urllib.request.Request(imageURL, headers=header)
-      raw_img = urllib.request.urlopen(req).read()
-      File = open(os.path.join(imagePath , dogName + "_" + str(counter) + "." + imageType), "wb")
+      imageURLs.append(imageURL)
+
+      # 403 에러가 뜨면 계속 진행
+      try:
+        req = urllib.request.Request(imageURL, headers=header)
+        raw_img = urllib.request.urlopen(req).read()
+      except urllib.request.HTTPError as e:
+        print (e.read())
+        continue
+      
+      File = open(os.path.join(imagePath , dogName + "_" + str(imageCount + 1) + "." + imageType), "wb")
       File.write(raw_img)
       File.close()
       successCounter = successCounter + 1
+      imageCount = imageCount + 1
+      print ("Succsessful Count:", successCounter)
+      break
+
+    if imageCount >= imageNumber:
+      imageCount = 0
       break
 
 
