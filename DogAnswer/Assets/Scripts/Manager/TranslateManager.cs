126d3c366a531f1aa43f5bddcb22f288b8bbaa76
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-12-21 20:18:20 +0900

    Update : 번역기 api 사용 시도
    안되니깐 중단

--- DogAnswer/Assets/Scripts/Manager/TranslateManager.cs
+++ DogAnswer/Assets/Scripts/Manager/TranslateManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
-using Google.Cloud.Translation;
+using Google.Cloud.Translation.V2;
 
 namespace DogAnswer
 {
     public class TranslateManager: Utility.Singleton<TranslateManager>
     {
-
+        public void test()
+        { 
+        }
     }
 }
