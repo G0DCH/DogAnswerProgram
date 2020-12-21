a98e18406a59d268e639f56efd5922e3310e6f42
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-12-21 18:47:41 +0900

    Add : 번역기, 너겟 패키지 설치

---
+++ DogAnswer/Assets/Scripts/Manager/TranslateManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using Google.Cloud.Translation;
+
+namespace DogAnswer
+{
+    public class TranslateManager: Utility.Singleton<TranslateManager>
+    {
+
+    }
+}
